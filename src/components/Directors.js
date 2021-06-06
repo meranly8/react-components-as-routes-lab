20185b408a5a966da4dae2e987c78de361e78a47
Michelle Ranly <meranly8@gmail.com>
2021-06-06 00:24:30 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { directors } from '../data';
+import React from 'react'
+import { directors } from '../data'
 
 const Directors = () => {
+  const renderDirectors = () => {
+    return directors.map(director => (
+      <div key={director.name}>
+        <h4>Name: {director.name}</h4>
+        <p>Movies</p>
+        <ul>
+          {director.movies.map(movie => (
+            <li key={movie}>{movie}</li>
+          ))}
+        </ul>
+      </div>
+    ))
+  }
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
-  );
+  )
 }
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { movies } from '../data';
+import React from 'react'
+import { movies } from '../data'
 
 const Movies = () => {
+  const renderMovies = () => {
+    return movies.map((movie) => (
+      <div key={movie.title}>
+        <h4>Name: {movie.title}</h4>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {movie.genres.map(genre => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    ))
+  }
+  
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
-  );
-};
+  )
+}
 
-export default Movies;
+export default Movies
